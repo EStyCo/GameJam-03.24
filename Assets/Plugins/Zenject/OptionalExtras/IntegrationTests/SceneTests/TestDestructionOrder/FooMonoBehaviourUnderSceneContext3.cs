257c9e26957d90a6255facb64cd44afde02953bd
EStyCo <dmerzlov3@gmail.com>
2024-02-19 17:03:22 +0800

    Накатил Zenject

---
+++ Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneTests/TestDestructionOrder/FooMonoBehaviourUnderSceneContext3.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Zenject.Tests.TestDestructionOrder
+{
+    public class FooMonoBehaviourUnderSceneContext3 : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
