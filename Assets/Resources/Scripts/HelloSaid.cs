efa51daba97c3635b5df8ba9c3a66d83af8250dd
said <kerorogunso@mail.ru>
2024-03-01 17:36:40 +0300

    sadsdsaa

--- Assets/Resources/Scripts/HelloSaid.cs
+++ Assets/Resources/Scripts/HelloSaid.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HelloSaid : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         
